a97cd1a384add68fac43e83bfcc0931680c1f21e
bseaverclc <bseaver@d155.org>
2024-07-27 11:47:11 -0500

    highlighted the better stat in gamestats

--- app/src/main/java/com/example/myfragmenttester/GameStatsAdapter.java
+++ app/src/main/java/com/example/myfragmenttester/GameStatsAdapter.java
@@ -1,6 +1,7 @@
 package com.example.myfragmenttester;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -52,46 +53,95 @@ public class GameStatsAdapter extends ArrayAdapter<ASet> {
             redPoints.setText("" + theSets.get(position).getRedStats().get("redScore"));
             bluePoints = rowView.findViewById(R.id.gameStatsBluePoints);
             bluePoints.setText("" + theSets.get(position).getBlueStats().get("blueScore"));
+            if( theSets.get(position).getRedStats().get("redScore")> theSets.get(position).getBlueStats().get("blueScore")){
+                redPoints.setBackgroundColor(Color.GREEN);
+            }
+            else if( theSets.get(position).getRedStats().get("redScore")< theSets.get(position).getBlueStats().get("blueScore")){
+                bluePoints.setBackgroundColor(Color.GREEN);
+            }
 
             redKills = rowView.findViewById(R.id.gameStatsRedKills);
             redKills.setText("" + theSets.get(position).getRedStats().get("Kill"));
             blueKills = rowView.findViewById(R.id.gameStatsBlueKills);
             blueKills.setText("" + theSets.get(position).getBlueStats().get("Kill"));
+            if( theSets.get(position).getRedStats().get("Kill")> theSets.get(position).getBlueStats().get("Kill")){
+             redKills.setBackgroundColor(Color.GREEN);
+            }
+             else if( theSets.get(position).getRedStats().get("Kill")< theSets.get(position).getBlueStats().get("Kill")){
+            blueKills.setBackgroundColor(Color.GREEN);
+            }
 
             redBlocks = rowView.findViewById(R.id.gameStatsRedBlocks);
             redBlocks.setText("" + theSets.get(position).getRedStats().get("Block"));
             blueBlocks = rowView.findViewById(R.id.gameStatsBlueBlocks);
             blueBlocks.setText("" + theSets.get(position).getBlueStats().get("Block"));
+            if( theSets.get(position).getRedStats().get("Block")> theSets.get(position).getBlueStats().get("Block")){
+                redBlocks.setBackgroundColor(Color.GREEN);
+            }
+            else if( theSets.get(position).getRedStats().get("Block")< theSets.get(position).getBlueStats().get("Block")){
+                blueBlocks.setBackgroundColor(Color.GREEN);
+            }
 
             redAces = rowView.findViewById(R.id.gameStatsRedAces);
             redAces.setText("" + theSets.get(position).getRedStats().get("Ace"));
             blueAces = rowView.findViewById(R.id.gameStatsBlueAces);
             blueAces.setText("" + theSets.get(position).getBlueStats().get("Ace"));
+            if( theSets.get(position).getRedStats().get("Ace")> theSets.get(position).getBlueStats().get("Ace")){
+                redAces.setBackgroundColor(Color.GREEN);
+             }
+            else if( theSets.get(position).getRedStats().get("Ace")< theSets.get(position).getBlueStats().get("Ace")){
+                blueAces.setBackgroundColor(Color.GREEN);
+            }
 
             redErrors = rowView.findViewById(R.id.gameStatsRedErrors);
             int redErrs = theSets.get(position).getBlueStats().get("Opponent Attack Err") - theSets.get(position).getBlueStats().get("Block");
             redErrs += theSets.get(position).getBlueStats().get("Opponent Serve Err") + theSets.get(position).getBlueStats().get("Opponent Err");
             redErrors.setText("" + redErrs);
-
             blueErrors = rowView.findViewById(R.id.gameStatsBlueErrors);
             int blueErrs = theSets.get(position).getRedStats().get("Opponent Attack Err") - theSets.get(position).getRedStats().get("Block");
             blueErrs += theSets.get(position).getRedStats().get("Opponent Serve Err") + theSets.get(position).getRedStats().get("Opponent Err");
             blueErrors.setText("" + blueErrs);
+            if( redErrs< blueErrs){
+                redErrors.setBackgroundColor(Color.GREEN);
+            }
+            else if( redErrs> blueErrs){
+                blueErrors.setBackgroundColor(Color.GREEN);
+            }
+
 
             redServiceErrors = rowView.findViewById(R.id.gameStatsRedServiceErrors);
             redServiceErrors.setText("" + theSets.get(position).getBlueStats().get("Opponent Serve Err"));
             blueServiceErrors = rowView.findViewById(R.id.gameStatsBlueServiceErrors);
             blueServiceErrors.setText("" + theSets.get(position).getRedStats().get("Opponent Serve Err"));
+            if(theSets.get(position).getBlueStats().get("Opponent Serve Err")<theSets.get(position).getRedStats().get("Opponent Serve Err")){
+                redServiceErrors.setBackgroundColor(Color.GREEN);
+            }
+            else  if(theSets.get(position).getBlueStats().get("Opponent Serve Err")>theSets.get(position).getRedStats().get("Opponent Serve Err")){
+                blueServiceErrors.setBackgroundColor(Color.GREEN);
+            }
+
 
             redAttackErrors = rowView.findViewById(R.id.gameStatsRedAttackErrors);
             redAttackErrors.setText("" + theSets.get(position).getBlueStats().get("Opponent Attack Err"));
             blueAttackErrors = rowView.findViewById(R.id.gameStatsBlueAttackErrors);
             blueAttackErrors.setText("" + theSets.get(position).getRedStats().get("Opponent Attack Err"));
+            if(theSets.get(position).getBlueStats().get("Opponent Attack Err")< theSets.get(position).getRedStats().get("Opponent Attack Err")){
+                redAttackErrors.setBackgroundColor(Color.GREEN);
+            }
+            else if(theSets.get(position).getBlueStats().get("Opponent Attack Err")> theSets.get(position).getRedStats().get("Opponent Attack Err")){
+                blueAttackErrors.setBackgroundColor(Color.GREEN);
+            }
 
             redAttacks = rowView.findViewById(R.id.gameStatsRedAttacks);
             redAttacks.setText("" + theSets.get(position).getRedAttack());
             blueAttacks = rowView.findViewById(R.id.gameStatsBlueAttacks);
             blueAttacks.setText("" + theSets.get(position).getBlueAttack());
+            if(theSets.get(position).getRedAttack()>theSets.get(position).getBlueAttack()){
+                redAttacks.setBackgroundColor(Color.GREEN);
+            }
+            else if(theSets.get(position).getRedAttack()<theSets.get(position).getBlueAttack()){
+                blueAttacks.setBackgroundColor(Color.GREEN);
+            }
 
             int rAtk = theSets.get(position).getRedAttack();
             int bAtk = theSets.get(position).getBlueAttack();
@@ -102,8 +152,9 @@ public class GameStatsAdapter extends ArrayAdapter<ASet> {
 
             redHitPct = rowView.findViewById(R.id.gameStatsRedHit);
 
+            double redPercent = 0;
             if (rAtk != 0) {
-                double redPercent = (double) (rKills - rAtkErrors) / rAtk;
+                 redPercent = (double) (rKills - rAtkErrors) / rAtk;
                 String redPercentString = String.format("%.3f", redPercent);
                 redHitPct.setText(redPercentString);
             } else {
@@ -112,21 +163,31 @@ public class GameStatsAdapter extends ArrayAdapter<ASet> {
 
             blueHitPct = rowView.findViewById(R.id.gameStatsBlueHit);
 
+            double bluePercent = 0;
             if (bAtk != 0) {
-                double bluePercent = (double) (bKills - bAtkErrors) / bAtk;
+                bluePercent = (double) (bKills - bAtkErrors) / bAtk;
                 String bluePercentString = String.format("%.3f", bluePercent);
                 blueHitPct.setText(bluePercentString);
             } else {
                 blueHitPct.setText("0.000");
             }
 
+            if(redPercent > bluePercent){
+                redHitPct.setBackgroundColor(Color.GREEN);
+            }
+            else if(redPercent < bluePercent){
+                blueHitPct.setBackgroundColor(Color.GREEN);
+            }
+
+
 
             redPassAvg = rowView.findViewById(R.id.gameStatsRedPass);
 
+            double redPassAvgNum = 0;
             int redTotal = theSets.get(position).getRedOne() + 2 * theSets.get(position).getRedTwo() + 3 * theSets.get(position).getRedThree();
             int redNum = theSets.get(position).getRedOne() + theSets.get(position).getRedTwo() + theSets.get(position).getRedThree() + theSets.get(position).getBlueStats().get("Ace");
             if (redNum != 0) {
-                double redPassAvgNum = redTotal / (double) redNum;
+                redPassAvgNum = redTotal / (double) redNum;
                 redPassAvg.setText(String.format("%.2f", redPassAvgNum));
             } else {
                 redPassAvg.setText("N/A");
@@ -136,11 +197,19 @@ public class GameStatsAdapter extends ArrayAdapter<ASet> {
 
             int blueTotal = theSets.get(position).getBlueOne() + 2 * theSets.get(position).getBlueTwo() + 3 * theSets.get(position).getBlueThree();
             int blueNum = theSets.get(position).getBlueOne() + theSets.get(position).getBlueTwo() + theSets.get(position).getBlueThree() + theSets.get(position).getRedStats().get("Ace");
+            double bluePassAvgNum = 0;
             if (blueNum != 0) {
-                double bluePassAvgNum = blueTotal / (double) blueNum;
+                bluePassAvgNum = blueTotal / (double) blueNum;
                 bluePassAvg.setText(String.format("%.2f", bluePassAvgNum));
             } else {
                 bluePassAvg.setText("N/A");
+            }
+
+            if(redPassAvgNum > bluePassAvgNum){
+                redPassAvg.setBackgroundColor(Color.GREEN);
+            }
+            else if(redPassAvgNum < bluePassAvgNum){
+                bluePassAvg.setBackgroundColor(Color.GREEN);
             }
 
 // sideout calculation
@@ -180,31 +249,51 @@ public class GameStatsAdapter extends ArrayAdapter<ASet> {
             blueSideoutPct = rowView.findViewById(R.id.gameStatsBlueSideout);
             redSideoutPct.setText(redPct + "%");
             blueSideoutPct.setText((bluePct + "%"));
+            if(redPct > bluePct){
+                redSideoutPct.setBackgroundColor(Color.GREEN);
+            }
+            else if(redPct < bluePct){
+                blueSideoutPct.setBackgroundColor(Color.GREEN);
+            }
 
 
             redDigs = rowView.findViewById(R.id.gameStatsRedDigs);
             blueDigs = rowView.findViewById(R.id.gameStatsBlueDigs);
             redDigs.setText("" + theSets.get(position).getRedDigs());
             blueDigs.setText("" + theSets.get(position).getBlueDigs());
+            if(theSets.get(position).getRedDigs() > theSets.get(position).getBlueDigs()){
+                redDigs.setBackgroundColor(Color.GREEN);
+            }
+            else if(theSets.get(position).getRedDigs() < theSets.get(position).getBlueDigs()){
+                blueDigs.setBackgroundColor(Color.GREEN);
+            }
 
 
             //percent earned code
             redPctEarned = rowView.findViewById(R.id.gameStatsRedPctEarned);
             bluePctEarned = rowView.findViewById(R.id.gameStatsBluePctEarned);
+            int rGoodPct = 0;
             if (theSets.get(position).getRedStats().get("redScore") != 0) {
                 int rGood = theSets.get(position).getRedStats().get("Ace") + rKills + theSets.get(position).getRedStats().get("Block");
-                int rGoodPct = (int) Math.round((double) rGood / theSets.get(position).getRedStats().get("redScore") * 100);
+                rGoodPct = (int) Math.round((double) rGood / theSets.get(position).getRedStats().get("redScore") * 100);
                 redPctEarned.setText(rGoodPct + "%");
             } else {
                 redPctEarned.setText("N/A");
             }
 
+            int bGoodPct = 0;
             if (theSets.get(position).getBlueStats().get("blueScore") != 0) {
                 int bGood = theSets.get(position).getBlueStats().get("Ace") + bKills + theSets.get(position).getBlueStats().get("Block");
-                int bGoodPct = (int) Math.round((double) bGood / theSets.get(position).getBlueStats().get("blueScore") * 100);
+                bGoodPct = (int) Math.round((double) bGood / theSets.get(position).getBlueStats().get("blueScore") * 100);
                 bluePctEarned.setText(bGoodPct + "%");
             } else {
                 bluePctEarned.setText("N/A");
+            }
+            if(rGoodPct>bGoodPct){
+                redPctEarned.setBackgroundColor(Color.GREEN);
+            }
+            else if(rGoodPct<bGoodPct){
+                bluePctEarned.setBackgroundColor(Color.GREEN);
             }
 
 
@@ -212,6 +301,8 @@ public class GameStatsAdapter extends ArrayAdapter<ASet> {
 
         return rowView;
     }
+
+
 
 
 
